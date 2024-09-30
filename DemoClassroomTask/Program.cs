8b0f1ce5a57801cae66dc84449165a15e935e598
Al Stone <astone@heckgrammar.co.uk>
2024-09-30 08:19:21 +0100

    Add project files.

---
+++ DemoClassroomTask/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DemoClassroomTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instructions are put into comments like this
+            // When asked to do a task, put it under the comment
+            // Write a line of code to output your name
+
+            
+            // make sure you commit and push your updated work
+            // Once done, take a screenshot of your program running
+            // add this to the readme
+        }
+    }
+}
